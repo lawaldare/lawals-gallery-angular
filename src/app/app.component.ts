6056ddda61920fa27edd7ec6e1bb9ab28280fb6e
dare lawal <darelawal@192.168.1.104>
2023-07-02 17:37:13 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,29 +14,33 @@ export class AppComponent implements OnInit {
   constructor(private uploadService: UploadService) {}
 
   ngOnInit(): void {
-    this.uploadService
-      .getFiles()
-      .snapshotChanges()
-      .pipe(
-        map((changes) => {
-          return changes.map((image) => ({
-            key: image.payload.key,
-            ...image.payload.val(),
-          }));
-        })
-      )
-      .subscribe((files) => {
-        this.files = files;
-      });
+    // this.uploadService
+    //   .getFiles()
+    //   .snapshotChanges()
+    //   .pipe(
+    //     map((changes) => {
+    //       return changes.map((image) => ({
+    //         key: image.payload.key,
+    //         ...image.payload.val(),
+    //       }));
+    //     })
+    //   )
+    //   .subscribe((files) => {
+    //     this.files = files;
+    //   });
 
-    setInterval(this.change.bind(this), this.numberOfSeconds);
+    // setInterval(this.change.bind(this), this.numberOfSeconds);
+    setInterval(this.change, this.numberOfSeconds);
   }
 
   change() {
     let div = document.querySelector('section');
-    let num = Math.floor(Math.random() * this.files.length) + 1;
+    let num = Math.floor(Math.random() * 231) + 1;
+    // console.log(this.files[num].url);
+
     if (div) {
-      div.innerHTML = `<img src='${this.files[num].url}'>`;
+      // div.innerHTML = `<img src='${this.files[num].url}'>`;
+      div.innerHTML = `<img src='assets/images/${num}.jpg'>`;
     }
   }
 }
